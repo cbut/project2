ef29ca28844da45b0b8b771cb8dc89f56383df20
dmdmdm1 <dayanamick1@gmail.com>
2019-09-21 17:14:12 +0200

    Adds Form for input and returns JSON results from Api call

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var PersonalityInsightsV3 = require('ibm-watson/personality-insights/v3');
+
+var personalityInsights = new PersonalityInsightsV3({
+    version: '2017-10-13',
+    url: 'https://gateway-fra.watsonplatform.net/personality-insights/api'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next){
+  const inputText = req.body.submittedText;
+  personalityInsights.profile(
+    {
+        content: inputText,
+        content_type: 'text/plain',
+        consumption_preferences: true
+    })
+    .then(result => {
+        console.log(JSON.stringify(result, null, 2));
+    })
+    .catch(err => {
+        console.log('error:', err);
+    }); 
+
+})
 
 module.exports = router;
