9937f5ab951b5ba2d19cc11f254cf2e78e3172d8
dmdmdm1 <dayanamick1@gmail.com>
2019-09-24 19:51:18 +0200

    Revert "Adds Basic authentication functionality"
    This reverts commit 73a1a8f70c375a0fc05024be58150312eedaf785.

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const session = require("express-session");
-
-const MongoStore = require("connect-mongo")(session);
-
-
-
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 const mongoose = require('mongoose');
-
-const dbName = 'personality_reports';
 
 // DB connect (and create DB)
 mongoose
-  .connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true })
+  .connect('mongodb://localhost/personality_reports', { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -27,30 +15,14 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-const indexRouter = require('./routes/index');
-const resultsRouter = require('./routes/results');
-const authRouter = require('./routes/auth');
+var indexRouter = require('./routes/index');
+var resultsRouter = require('./routes/results');
 
-const app = express();
-
-app.use(session({
-  secret: "abc", // ASK ABOUT THIS SECRET
-  store: new MongoStore({ // this is going to create the `sessions` collection in the db
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
-  })
-}));
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-require('./config/passport.js')
-
-
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -60,7 +32,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/results', resultsRouter);
-app.use('/auth', authRouter);
 
 
 
