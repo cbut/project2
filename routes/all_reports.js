be076fda68360004f4a89e53fc915fb7cb736131
Christoph Butenschön <c.butenschoen@gmail.com>
2019-09-26 21:03:44 +0200

    adds edit function to the notes of a report

--- routes/all_reports.js
+++ routes/all_reports.js
@@ -4,37 +4,37 @@ var router = express.Router();
 const User = require("../models/user");
 
 /* displays results */
-router.get("/", function(req, res, next) {
-  User.find({}).then(allTheData => {
-    console.log(allTheData);
-    res.render("all_reports/index", { reports: allTheData });
-  });
-  // res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+    User.find({}).then(allTheData => {
+        console.log(allTheData);
+        res.render("all_reports/index", { reports: allTheData });
+    });
+    // res.send('respond with a resource');
 });
 
 // GET /all_reports/:report_id/delete
-router.get("/:report_id/delete", function(req, res, next) {
-  const toBeDeleted = req.params.report_id;
-  User.findByIdAndRemove(toBeDeleted).then(toBeDeleted => {
-    res.redirect("/all_reports/");
-  });
+router.get("/:report_id/delete", function (req, res, next) {
+    const toBeDeleted = req.params.report_id;
+    User.findByIdAndRemove(toBeDeleted).then(toBeDeleted => {
+        res.redirect("/all_reports/");
+    });
 });
 
-// GET /all_reports/:report_id/edit
-router.get("/:report_id/edit", function(req, res, next) {
-  User.findById(req.params.room_id).then(report => {
-    console.log(report);
-    res.render("all_reports/edit", { report });
-  });
+// GET /all_reports/:report_id/edit  => edit page for the "note" of a report
+router.get("/:report_id/edit", function (req, res, next) {
+    User.findById(req.params.report_id).then(report => {
+        console.log(report);
+        res.render("all_reports/edit", { report });
+    });
 });
 
-// POST /all_reports/:room_id
-router.post("/:report_id", function(req, res, next) {
-  let opennessLocationInDatabase = req.params.report_id;
-  let { opennessPercentile } = req.body;
-  console.log(opennessLocationInDatabase + "  " + opennessPercentile);
-  // User.findByIdAndUpdate(req.params.report_id, {opennessPercentile}).then(() => {
-  // }
+// POST / all_reports /: report_id    => POST from Edit page, updating the "note" of a report
+router.post("/:report_id", function (req, res, next) {
+    let { note } = req.body
+    User.findByIdAndUpdate(req.params.report_id, { note }).then(() => {
+        res.redirect("/all_reports/");
+    })
 });
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,38 +5,34 @@ var PersonalityInsightsV3 = require("ibm-watson/personality-insights/v3");
 const User = require("../models/user");
 
 var personalityInsights = new PersonalityInsightsV3({
-  version: "2017-10-13",
-  url: "https://gateway-fra.watsonplatform.net/personality-insights/api"
+    version: "2017-10-13",
+    url: "https://gateway-fra.watsonplatform.net/personality-insights/api"
 });
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express" });
 });
 
-router.post("/", function(req, res, next) {
-  const inputText = req.body.submittedText;
-  personalityInsights
-    .profile({
-      content: inputText,
-      content_type: "text/plain",
-      consumption_preferences: true
-    })
-    .then(result => {
-      console.log(result);
-      console.log(result.word_count);
-      User.create({ reports: [result] });
-      res.render("results", result);
-      // console.log(JSON.stringify(result, null, 2));
-    })
-    .catch(err => {
-      console.log("error:", err);
-    });
+router.post("/", function (req, res, next) {
+    const inputText = req.body.submittedText;
+    personalityInsights
+        .profile({
+            content: inputText,
+            content_type: "text/plain",
+            consumption_preferences: true
+        })
+        .then(result => {
+            console.log(result);
+            console.log(result.word_count);
+            User.create({ reports: [result] });
+            res.render("results", result);
+            // console.log(JSON.stringify(result, null, 2));
+        })
+        .catch(err => {
+            console.log("error:", err);
+        });
 });
 
-// POST /rooms/:room_id
-router.post("/:room_id", function(req, res, next) {
-  res.send("this works");
-});
 
 module.exports = router;
