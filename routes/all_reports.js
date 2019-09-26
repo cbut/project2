bb41c85363edc98adf5ce46621989653343dfb95
cbut <c.butenschoen@gmail.com>
2019-09-26 17:32:06 +0200

    adds ability to delete single reports from the reports overview

--- routes/all_reports.js
+++ routes/all_reports.js
@@ -1,16 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const User = require('../models/user')
+const User = require("../models/user");
 
 /* displays results */
-router.get('/', function (req, res, next) {
-    User.find({}).then((allTheData) => {
-        console.log(allTheData)
-        res.render('all_reports/index', { reports: allTheData })
+router.get("/", function(req, res, next) {
+  User.find({}).then(allTheData => {
+    console.log(allTheData);
+    res.render("all_reports/index", { reports: allTheData });
+  });
+  // res.send('respond with a resource');
+});
 
-    })
-    // res.send('respond with a resource');
+// GET /rooms/:room_id/edit
+router.get("/:report_id/delete", function(req, res, next) {
+  const toBeDeleted = req.params.report_id;
+  User.findByIdAndRemove(toBeDeleted).then(toBeDeleted => {
+    res.redirect("/all_reports/");
+  });
 });
 
 module.exports = router;
